2a77d503d91162c8ec47012cb6cc33f199a7ce15
ngocdu <ngocdu@ngocdu-K53E.(none)>
2013-04-15 14:35:59 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TalentShop::Application.config.secret_token = '1b4054231eb6b373797e4697827f2ab7f40397373ab731084f1cb1cd537bb5b385538b56eb5915e540541f90a7fec334aba178a36270653647dae46386e8b038'
